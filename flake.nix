2396e78f592afeebfd2d2d1933a9e009b5be15ce
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:33 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Akeyless Python SDK - auto-generated Python client for the Akeyless API";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = { url = "github:pleme-io/substrate"; inputs.nixpkgs.follows = "nixpkgs"; };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, substrate, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+      mkPythonPackage = (import "${substrate}/lib/python-package.nix").mkPythonPackage;
+    in {
+      packages.default = mkPythonPackage pkgs {
+        pname = "akeyless-python";
+        version = "0.0.0-dev";
+        src = self;
+        description = "Akeyless Python SDK - auto-generated Python client for the Akeyless API";
+      };
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ python3 python3Packages.pip ];
+      };
+    });
+}
