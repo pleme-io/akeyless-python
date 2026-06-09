89ec766dfcc9e258b56269748d43d5898262b4a8
drzln <luis.d@akeyless.io>
2026-06-08 17:53:53 -0700

    flake: follow substrate/nixpkgs (fleet nixpkgs alignment for cache reuse)

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   description = "Akeyless Python SDK - auto-generated Python client for the Akeyless API";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    substrate = { url = "github:pleme-io/substrate"; inputs.nixpkgs.follows = "nixpkgs"; };
+    nixpkgs.follows = "substrate/nixpkgs";
+    substrate = { url = "github:pleme-io/substrate";};
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
